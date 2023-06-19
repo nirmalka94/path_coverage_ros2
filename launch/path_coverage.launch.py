b3915f3ba65923a8f2f650cfca307b7d269763d8
hazeezadebayo <32035140+hazeezadebayo@users.noreply.github.com>
2023-06-19 15:05:30 +0300

    Update path_coverage.launch.py
    flexible number of mid waypoints can now be inserted between distant coverage generated waypoints.

--- launch/path_coverage.launch.py
+++ launch/path_coverage.launch.py
@@ -16,10 +16,12 @@ def generate_launch_description():
             parameters=[{
                 "boustrophedon_decomposition": True, # <!-- Whether to execute the boustrophedon decomposition --> default - "true"
                 "border_drive": False, # <!-- Drive around the cell first --> default - "false"
-                "robot_width": 0.40, # <!-- Width of each path --> default - "0.15"
-                "costmap_max_non_lethal": 70, # <!-- Maximum costmap value to consider free --> default - "15"
+                "robot_width": 0.60, # <!-- Width of each path --> default - "0.15"
+                "costmap_max_non_lethal": 15, # <!-- Maximum costmap value to consider free --> default - "15"
                 "base_frame": "base_link", # <!-- The robots base frame --> default - "base_link"
-                "global_frame": "map"
+                "global_frame": "map",
+                "split_wp_dist": 3.5,       # <!-- The maximum distance between waypoints before a split can be considered --> default - "3.0"
+                "num_midpoints": 2          # <!-- The number of waypoints to put between any two distant waypoints --> default - "2"     
             }]
         ),
     ])
